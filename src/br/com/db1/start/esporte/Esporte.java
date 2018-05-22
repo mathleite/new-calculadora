753ba9b206a6145da99d54fc4f2185632f48a5cb
Aluno <Aluno>
2018-05-22 16:14:25 -0300

    Exemplo de Strategy

---
+++ src/br/com/db1/start/esporte/Esporte.java
@@ -0,0 +1,30 @@
+package br.com.db1.start.esporte;
+
+public class Esporte {
+	
+	private EsporteOlimpicoColetivo nomeEsporte;
+	
+	private Integer tempoJogo;
+
+	public EsporteOlimpicoColetivo getNomeEsporte() {
+		return nomeEsporte;
+	}
+
+	public void setNomeEsporte(EsporteOlimpicoColetivo nomeEsporte) {
+		this.nomeEsporte = nomeEsporte;
+	}
+
+	public Integer getTempoJogo() {
+		return tempoJogo;
+	}
+
+	public void setTempoJogo(Integer tempoJogo) {
+		this.tempoJogo = tempoJogo;
+	}
+
+	public void retornarDadosDoEsporte(){
+		
+		
+		
+	}
+}
